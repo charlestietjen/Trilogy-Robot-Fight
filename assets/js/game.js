6633de626c7748ef82728907c0c1604628e3beb0
Charles T <tietjen.charles@gmail.com>
2021-11-29 22:55:21 -0500

    commit

--- assets/js/game.js
+++ assets/js/game.js
@@ -143,6 +143,21 @@ var endGame = function() {
     window.alert("You've lost your robot in battle!");
   }
 
+  var highScore = localStorage.getItem("highscore");
+  if (highScore === null) {
+    highScore = 0;
+  }
+  
+  if (playerInfo.money > highScore) {
+    localStorage.setItem("highscore", playerInfo.money);
+    localStorage.setItem("name", playerInfo.name);
+  
+    alert(playerInfo.name + " now has the high score of " + playerInfo.money + "!");
+  }
+  else {
+    alert(playerInfo.name + " did not beat the high score of " + highScore + ". Maybe next time!");
+  }
+
   // ask player if they'd like to play again
   var playAgainConfirm = window.confirm('Would you like to play again?');
 
